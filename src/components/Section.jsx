abc29dcd02752db7c00766d530f507607ec5b532
mikaila <mikailamohammad23@gmail.com>
2026-04-16 09:31:44 +0100

    second one

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -2,7 +2,12 @@
 export function Section()  {
    return (
     <div className="section">
+    <div className="heading-section">
     <h1>Welcome to my website</h1>
+    <p>This is my second portfolio an my eigth months experience
+    please support me.
+    </p>
+    </div>
     </div>
    )
 }
