9c973aeb4fae9bf66d1a296df7e5956ca36240b3
mikaila <mikailamohammad23@gmail.com>
2026-04-19 09:37:52 +0100

    fola

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Section } from './components/Section';
 import { About } from './components/About';
 import { Header } from './Header';
+import { Feature }from './components/Feature'
 
 
 function App() {
